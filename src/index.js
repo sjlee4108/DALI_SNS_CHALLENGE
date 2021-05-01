e87bc36ae95ea933894c4871a066e98fd4903d45
Seungjae Lee <sjlee4108@gmail.com>
2021-05-01 11:17:12 -0400

    part1

--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
-import {
-  BrowserRouter as Router, Route, NavLink, Switch,
-} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const About = () => <div> All there is to know about me </div>;
-const Welcome = () => <div>Welcome</div>;
-const Test = (props) => <div> ID: {props.match.params.id} </div>;
-const FallBack = () => <div>URL Not Found</div>;
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route path="/about" component={About} />
-        <Route exact path="/test/:id" component={Test} />
-        <Route component={FallBack} />
-      </Switch>
-    </div>
-  </Router>
+import reducers from './reducers';
+
+import App from './components/app';
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
+
+// we now wrap App in a Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('main'),
 );
-
-const Nav = () => (
-  <nav>
-    <ul>
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About</NavLink></li>
-      <li><NavLink to="/test/id1">test id1</NavLink></li>
-      <li><NavLink to="/test/id2">test id2</NavLink></li>
-    </ul>
-  </nav>
-);
-
-ReactDOM.render(<App />, document.getElementById('main'));
